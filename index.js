336006f4848682a548101e53cad7fad58cdfb7d1
Sean Febo <seanfebo@techie.com>
2018-03-18 23:12:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if(cardNumber === 0) {
+  if(!cardNumber) {
     return ("Sorry, we don't have a credit card on file for you.")
   }
 }
